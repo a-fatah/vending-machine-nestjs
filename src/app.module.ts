1698dc5a23a903c054213d3041be0e2559d41647
a-fatah <abdulfatah66@gmail.com>
2024-02-23 03:46:35 +0100

    externalize database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,19 +7,23 @@ import { APP_FILTER } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: 's3cr3t',
-      username: 'dev',
-      database: 'vending-machine',
-      synchronize: true,
-      logging: true,
-      entities: ['dist/**/*.entity.js'],
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT) || 5432,
+        password: process.env.DB_PASSWORD,
+        username: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        logging: true,
+        entities: ['dist/**/*.entity.js'],
+      }),
     }),
     UserModule,
     AuthModule,
